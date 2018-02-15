a56d0d86b3419199525d1435646843fa9508648a
Albin <albinso@kth.se>
2018-02-15 01:59:19 +0100

    Votable languages

--- language_rater/views.py
+++ language_rater/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import get_template
 from language_rater.models import Language
+from language_rater.forms import VoteForm
 
 # Create your views here.
 def index(request):
 	langs = Language.objects.all() # ModelManager, keeps track of model instances in database. Here, get all languages
 	return render(request, "language_rater/vote_page.html", {'langs': langs})
+
+def upvote(request, lang_id):
+	language = Language.objects.get(pk=lang_id)
+	language.vote()
+	language.save()
+	return HttpResponseRedirect("/rate")
--- workshop/urls.py
+++ workshop/urls.py
@@ -20,4 +20,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rate/', language_rater.views.index),
+    path('rate/upvote/<lang_id>/', language_rater.views.upvote),
 ]
