cb6c166323643b4f6eb1f40675974fbf9f2dcdfb
Albin <albinso@kth.se>
2018-02-15 01:03:38 +0100

    Simple model

--- language_rater/models.py
+++ language_rater/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Language(models.Model):
+
+	num_votes = models.IntegerField()
+	name = models.TextField()
+
+	def vote(self):
+		self.num_votes += 1
