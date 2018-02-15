7a7e190b5439a4f1d862ebc21928920713c021b7
Albin <albinso@kth.se>
2018-02-15 01:23:19 +0100

    Display languages and votes

--- language_rater/admin.py
+++ language_rater/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from language_rater.models import Language
 # Register your models here.
+admin.site.register(Language)
--- language_rater/views.py
+++ language_rater/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import get_template
+from language_rater.models import Language
 
 # Create your views here.
 def index(request):
-	return render(request, "language_rater/vote_page.html")
+	langs = Language.objects.all() # ModelManager, keeps track of model instances in database. Here, get all languages
+	return render(request, "language_rater/vote_page.html", {'langs': langs})
